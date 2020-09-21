baca10b1b679296a2191d7f0ff7efad7ac8296f6
90262 <70868307+jacob850920@users.noreply.github.com>
2020-09-21 08:39:33 +0800

    Add files via upload

--- 90262.py
+++ 90262.py
@@ -7,19 +7,19 @@ Created on Tue Sep 15 15:41:32 2020
 
 string="I love you"
 res=string.split(" ")
-print(res)
+print(res[0],res[1],res[2])
 
 ##############################
 string2=string[::-1]
 res=string2.split(" ")
-print(res)
+print(res[0],res[1],res[2])
 
 ##############################
 stringso="so "
 stringmuch="much"
-print(string,stringso*100,stringmuch)
+print(string,stringso*100+stringmuch)
 
 ##############################
 import random
 a=random.randint(1, 100)
-print(string,stringso*a,stringmuch)
+print(string,stringso*a+stringmuch)
